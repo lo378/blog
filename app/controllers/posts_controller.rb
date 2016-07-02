cd75fe0d87c60a8b00ef9a0d9be4027e608dee87
lo378 <mikeal3789@gmail.com>
2016-07-03 00:34:04 +0100

    edit and delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		if @post.update(doc_params)
+		if @post.update(post_params)
 			redirect_to @post
 		else
 			render 'edit'
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
 
 	def destroy
 		@post.destroy
-		redirect_to posts_path
+		redirect_to root_path
 	end
 
 	private
